6cf2d1ebe5db37b695e623db6df2ca8e8622b2cd
Isaac <isaac@Isaacs-Mac-mini.local>
2019-12-09 18:38:43 +0000

    Tidy up code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
